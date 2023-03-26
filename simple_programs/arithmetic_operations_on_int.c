a87ea0757ee25026f04a15b189e3751416a4cbae
Amit Raj <rajamitnine@gmail.com>
2023-03-26 21:31:24 +0530

    A program to understand 'Arithmetic Operations' on int data type

---
+++ simple_programs/arithmetic_operations_on_int.c
@@ -0,0 +1,16 @@
+// A program to understand 'Arithmetic Operations' on int data type :
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 5;
+    int y = 2;
+
+    printf("%d\n", (x + y));
+    printf("%d\n", (x - y));
+    printf("%d\n", (x * y));
+    printf("%d\n", (x / y));
+
+    return 0;
+}
