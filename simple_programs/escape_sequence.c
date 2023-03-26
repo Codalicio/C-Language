af1dc1a209e19d3ecc6b05ac67c9fd2b14302130
Amit Raj <rajamitnine@gmail.com>
2023-03-26 19:57:35 +0530

    A program to understand the escape sequence '\n'

---
+++ simple_programs/escape_sequence.c
@@ -0,0 +1,11 @@
+// A program to understand the escape sequence '\n' :
+
+#include <stdio.h>
+
+int main()
+{
+    printf("nn\n\nnn\n"); 
+    printf("nn/n/nnn/n"); // -> nn/n/nnn/n
+
+    return 0;
+}
