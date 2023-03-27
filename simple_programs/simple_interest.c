43f7613ffe4d2c3b266537275057e0240c4e8015
Amit Raj <rajamitnine@gmail.com>
2023-03-27 16:04:25 +0530

    A program to find the simple interest when principal, rate and time are given

---
+++ simple_programs/simple_interest.c
@@ -0,0 +1,23 @@
+// A program to find the simple interest when principal, rate of interest and time is given :
+
+#include <stdio.h>
+
+int main()
+{
+    float principal, rate, time, si;
+
+    printf("Enter the principal amount : ");
+    scanf("%f", &principal);
+
+    printf("Enter the rate of interest : ");
+    scanf("%f", &rate);
+
+    printf("Enter time in years : ");
+    scanf("%f", &time);
+
+    si = ((principal * rate * time) / 100);
+
+    printf("%f is the simple interest on principal amount of %f with the rate of as %f percent in time %f years!", si, principal, rate, time);
+
+    return 0;
+}
